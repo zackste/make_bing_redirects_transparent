01d4fe4716b6588fc48195dcf32924274121cde8
Zack <4434856+zackste@users.noreply.github.com>
2026-07-24 08:53:47 -0700

    Initial commit. Working as is. (#1)
    No reviewers on the initial commit since I'm the only one on this project so far. This is going in to unblock additional progress.

---
+++ src/bingMonitor.ts
@@ -0,0 +1,53 @@
+// Copyright (c) 2026 Zack Stephens
+
+const anchorSelector = "main a[href^=http][h]";
+const anchorAncestorSelector = "li";
+
+function decodeLink(element: HTMLAnchorElement): string | null {
+  const href = element.getAttribute("href");
+  const url = new URL(href || "", "about://blank");
+  if (!url.hostname.match(/\.bing\.com$/)) {
+    return null;
+  }
+  if (url.pathname !== "/ck/a") {
+    return null;
+  }
+  const params = new URLSearchParams(url.search);
+  const u = params.get("u");
+  if (!u) {
+    return null;
+  }
+  // The url seems to start with a couple invalid characters, so we trim them off. The first two characters are always "a1", but the next two characters can vary. So we just trim off the first two characters.
+  let cleanedUpUrl = u.substring(2);
+  let modulus = cleanedUpUrl.length % 4;
+  if (modulus > 0) {
+    cleanedUpUrl += "=".repeat(4 - modulus);
+  }
+  cleanedUpUrl = cleanedUpUrl.replace(/-/g, "+").replace(/_/g, "/");
+  const decodedUrl = atob(cleanedUpUrl);
+  const realUrl = decodeURIComponent(decodedUrl);
+  return realUrl;
+}
+
+function tryUpdateAnchorSectionTitle(element: HTMLAnchorElement) {
+  const realUrl = decodeLink(element);
+  if (realUrl) {
+    element.closest(anchorAncestorSelector)?.setAttribute("title", realUrl);
+  }
+}
+
+document.querySelectorAll(anchorSelector).forEach((element) => {
+  tryUpdateAnchorSectionTitle(element as HTMLAnchorElement);
+});
+
+const anchorObserver = new MutationObserver(mutations => {
+  for (const mutation of mutations) {
+    for (const node of mutation.addedNodes) {
+      if (node.nodeType === Node.ELEMENT_NODE && (node as HTMLElement).tagName === 'A' && (node as HTMLElement).matches(anchorSelector)) {
+        tryUpdateAnchorSectionTitle(node as HTMLAnchorElement);
+      }
+    }
+  }
+});
+
+anchorObserver.observe(document.body, { childList: true, subtree: true });
